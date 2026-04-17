35cd6dd80cbb08a5cc1495ae7a07d824f1b2b913
SkiFlye <matvey.nikiforoff@yandex.ru>
2026-04-17 17:28:16 +0500

    pom

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import asyncio
+import threading
+from proxy import waf_proxy
+from app import app
+
+
+def run_flask():
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
+
+
+def run_proxy():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(waf_proxy.start())
+    loop.run_forever()
+
+
+if __name__ == '__main__':
+    print("Запуск WAF сервиса...")
+    print("Flask сайт: http://127.0.0.1:5000")
+    print("WAF прокси: http://0.0.0.0:8080")
+    print()
+
+    proxy_thread = threading.Thread(target=run_proxy, daemon=True)
+    proxy_thread.start()
+
+    run_flask()
